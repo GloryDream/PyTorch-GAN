9c56bd077ac7fe105697efd4f85a1997e817db28
neogong_Mac <xy_Gong@foxmail.com>
2018-10-30 16:49:23 -0500

    support tensorboard

--- implementations/acgan/acgan_celeba.py
+++ implementations/acgan/acgan_celeba.py
@@ -2,19 +2,22 @@ import argparse
 import os
 import numpy as np
 import math
+import datetime
 
 import torchvision.transforms as transforms
-from torchvision.utils import save_image
+from torchvision.utils import make_grid
 
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torch.autograd import Variable
+from tensorboardX import SummaryWriter
 
+import dateutil.tz
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
-os.makedirs('images', exist_ok=True)
+os.makedirs('output', exist_ok=True)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
@@ -25,13 +28,30 @@ parser.add_argument('--b2', type=float, default=0.999, help='adam: decay of firs
 parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
 parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
 parser.add_argument('--n_classes', type=int, default=10, help='number of classes for dataset')
-parser.add_argument('--img_size', type=int, default=32, help='size of each image dimension')
+parser.add_argument('--img_size', type=int, default=64, help='size of each image dimension')
 parser.add_argument('--channels', type=int, default=1, help='number of image channels')
 parser.add_argument('--sample_interval', type=int, default=400, help='interval between image sampling')
+parser.add_argument('--name', type=str, help='the name of the model')
+
 opt = parser.parse_args()
 print(opt)
 
 cuda = True if torch.cuda.is_available() else False
+
+# prepare dir
+now = datetime.datetime.now(dateutil.tz.tzlocal())
+timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
+model_name = opt.name
+output_dir = os.path.join('output', model_name)
+logdir_path = os.path.join(output_dir, 'log', timestamp)
+model_path = os.path.join(output_dir, 'model')
+
+# mkdir
+os.makedirs(output_dir)
+os.makedirs(logdir_path)
+os.makedirs(model_path)
+TB = SummaryWriter(log_dir=logdir_path)
+
 
 def weights_init_normal(m):
     classname = m.__class__.__name__
@@ -131,6 +151,7 @@ os.makedirs('../../data/mnist', exist_ok=True)
 dataloader = torch.utils.data.DataLoader(
     datasets.MNIST('../../data/mnist', train=True, download=True,
                    transform=transforms.Compose([
+                        transforms.CenterCrop(160),
                         transforms.Resize(opt.img_size),
                         transforms.ToTensor(),
                         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
@@ -144,6 +165,7 @@ optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt
 FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 
+
 def sample_image(n_row, batches_done):
     """Saves a grid of generated digits ranging from 0 to n_classes"""
     # Sample noise
@@ -152,7 +174,23 @@ def sample_image(n_row, batches_done):
     labels = np.array([num for _ in range(n_row) for num in range(n_row)])
     labels = Variable(LongTensor(labels))
     gen_imgs = generator(z, labels)
-    save_image(gen_imgs.data, 'images/%d.png' % batches_done, nrow=n_row, normalize=True)
+
+    def log_image(tensor, filename, nrow=8, padding=2,
+                   normalize=False, range=None, scale_each=False, pad_value=0):
+        """Log a given Tensor into an image file.
+
+        Args:
+            tensor (Tensor or list): Image to be saved. If given a mini-batch tensor,
+                saves the tensor as a grid of images by calling ``make_grid``.
+            **kwargs: Other arguments are documented in ``make_grid``.
+        """
+        from PIL import Image
+        grid = make_grid(tensor, nrow=nrow, padding=padding, pad_value=pad_value,
+                         normalize=normalize, range=range, scale_each=scale_each)
+        ndarr = grid.mul(255).clamp(0, 255).byte().permute(1, 2, 0).cpu().numpy()
+        TB.add_image(filename + '/iters{}'.format(epoch), ndarr, batches_done)
+
+    log_image(gen_imgs.data, 'imgs', nrow=n_row, normalize=True)
 
 # ----------
 #  Training
